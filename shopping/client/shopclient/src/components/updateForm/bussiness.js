aacf150148b9e6748c1e6123c2ff116e4055f559
Hunter <313267717@qq.com>
2020-06-08 11:30:28 +0800

    注册前后端，修改个人信息前端完结

--- shopping/client/shopclient/src/components/updateForm/bussiness.js
+++ shopping/client/shopclient/src/components/updateForm/bussiness.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import config from "../../config/config"
+import {
+  Toast
+} from "mint-ui";
 const {
   ServerApi,
   StorageName,
@@ -11,6 +14,28 @@ export default class UpdateBussiness extends Vue {
     this.vueComponent = _vueComponent
   }
   submitData() {
-    console.log('aaa')
+    for (const key in this.vueComponent.userInfo) {
+      if (!this.vueComponent.userInfo[key].length && this.vueComponent.userInfo[key] != true && this.vueComponent.userInfo[key] != 0) {
+        Toast('请填写完整的信息');
+        return
+      }
+    }
+    console.log(this.vueComponent.userInfo)
+    this.$axios
+      .post(ServerApi.user.updateUser, {
+        crypto: this.$crypto.setCrypto({
+          token: this.$storage.getStorage(StorageName.Token),
+          ...this.vueComponent.userInfo
+        })
+      }).then(res => {
+        switch (res.result) {
+          case 1:
+            Toast(res.msg);
+            history.go(-1)
+            break;
+          default:
+            break;
+        }
+      })
   }
 }
