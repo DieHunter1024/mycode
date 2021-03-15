6047a300518630fac7a2e29e2ca9bc26a23413f7
Hunter <313267717@qq.com>
2021-03-15 10:44:32 +0800

    优化计时器

--- csdn自动点赞评论/复制到控制台.js
+++ csdn自动点赞评论/复制到控制台.js
@@ -1,7 +1,22 @@
-let index = 0
- setInterval(function(){
-if(index>20){
-    return
+let index = 0 //第几条
+let maxPage = 30 //总页数
+let page = 5 //5页停止一次
+let timeTick //计时器
+init()
+
+function init() {
+    timeTick = setInterval(function () {
+        if (index > maxPage) {
+            clearInterval(timeTick)
+            return
+        }
+        if (index % page == 0) {
+            clearInterval(timeTick)
+            setTimeout(function (params) { //暂停60秒
+                init()
+            }, 60 * 1000)
+            return
+        }
+        document.querySelector('#feedlist_id').querySelectorAll('.clearfix')[index++].querySelector('a').click()
+    }, 5000)
 }
-document.querySelector('#feedlist_id').querySelectorAll('.clearfix')[index++].querySelector('a').click() 
-},5000)
