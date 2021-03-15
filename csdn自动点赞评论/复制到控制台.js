4befd4fc9ff3f4d89b90a013d87901e941a2873d
Hunter <313267717@qq.com>
2021-03-15 17:48:37 +0800

    优化按键打开

--- csdn自动点赞评论/复制到控制台.js
+++ csdn自动点赞评论/复制到控制台.js
@@ -1,9 +1,10 @@
 let index = 0 //第几条博客
-let maxPage = 30 //博客总数
+let maxPage = 3 //博客总数
 let page = 5 //5页停止一次，防止检测评论太快
 let time = 30 * 1000 //延时评论
 let speed = 3 * 1000 //评论速度
 let timeTick //计时器
+let elements = document.querySelector('#feedlist_id').querySelectorAll('.clearfix') //博客列表
 init()
 
 function init() {
@@ -14,8 +15,20 @@ function init() {
         }
         if (index && index % page == 0) {
             clearInterval(timeTick)
+            pageList = clearPage(pageList)
             setTimeout(init, time)
         }
-        document.querySelector('#feedlist_id').querySelectorAll('.clearfix')[index++].querySelector('a').click()
+        let hrefItem = elements[index++].querySelector('a').href
+        window.open(hrefItem)
     }, speed)
+}
+
+function keyDownCtrl() {
+    let mockKeyboardEvent = new KeyboardEvent('keydown', {
+        ctrlKey: true
+    });
+    document.dispatchEvent(mockKeyboardEvent);
+}
+document.onkeypress=function (params) {
+    console.log('ctrl')
 }
