18eabc85cb12e79f899c839bda4dfb30759deef6
Hunter <313267717@qq.com>
2021-03-23 23:49:20 +0800

    git空文件夹提交

---
+++ git批量提交空文件夹/git批量提交空文件夹.bat
@@ -0,0 +1,15 @@
+@echo off&echo 查找空文件夹
+
+set dd=%~1
+
+if "%~1"=="" set/p dd=输入或拖入要查找的总文件夹：
+
+cd/d "%dd%"
+
+for /f "delims=" %%a in ('dir/b/s/ad')do (
+
+  dir/a/s/b "%%a"|find/v "">nul||(set/a "fn+=1"&echo.路径 %%a 文件夹名 %%~nxa&type nul>>%%a\gitkeep))
+
+if defined fn (echo 共找到 %fn% 个空文件夹，已创建gitkeep空文件)else echo 没找到空文件夹
+
+pause 
---
+++ git批量提交空文件夹/批量删除某个文件.bat
@@ -0,0 +1,8 @@
+@echo off&echo 批量删除相同文件
+if "%~1"=="" set/p dd=输入或拖入要删除的文件夹
+
+cd/d "%dd%"
+
+del /s gitkeep
+
+pause
