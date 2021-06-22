dc055ab9a62c239027faf3f31936b1e9cb8de7a2
zhangboyu <zhangboyu@hosecloud.com>
2021-06-22 14:52:40 +0800

    调度中心实现

--- Vue原理/dep.js
+++ Vue原理/dep.js
@@ -1,4 +1,14 @@
-// 调度中心
-class Dep{
-    
+// 调度中心（观察者模式）
+class Dep {
+    constructor() {
+        this.observerList = []
+    }
+    fireEvent(e) {
+        this.observerList.forEach(item => {
+            item.update(e)
+        })
+    }
+    subscribe(fn) {
+        this.observerList.push(fn)
+    }
 }
