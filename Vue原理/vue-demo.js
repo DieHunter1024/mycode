119d2570db8bfbcc4af34693283d985e997cb878
zhangboyu <zhangboyu@hosecloud.com>
2021-06-22 14:30:14 +0800

    接入vue

--- Vue原理/vue-demo.js
+++ Vue原理/vue-demo.js
@@ -1,7 +1,14 @@
+import DataProxy from './proxy.js'
 import Compile from './compile.js'
 class VueDemo {
     constructor(options) {
-        this.compile = new Compile(options.elem)
+        this.data = options.data
+        this.el = options.el
+        this.options = options
+        if (this.el) {
+            this.compile = new Compile(options.el, this)
+            this.proxy = new DataProxy(this.data, this)
+        }
     }
 }
 export default VueDemo
