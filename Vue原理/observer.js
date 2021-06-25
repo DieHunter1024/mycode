3b8d3e690a1b3d4274d0bee70f4daf9afe835d20
zhangboyu <zhangboyu@hosecloud.com>
2021-06-25 18:13:18 +0800

    初始化observer

--- Vue原理/observer.js
+++ Vue原理/observer.js
@@ -1,4 +1,13 @@
 // 发布模式
-class Observer{
-    
+class Observer {
+    constructor(data) {
+        this.initObserver(data)
+    }
+    initObserver(data = {}) {
+        
+    }
+    // 递归监听所有层级
+    newReactive(data, key, val) {
+        
+    }
 }
