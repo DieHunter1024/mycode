1ee3ed199d7d044d587b4fda3500db7af14a062e
zhangboyu <zhangboyu@hosecloud.com>
2021-07-04 19:52:57 +0800

    将页面多个元素导出为pdf

--- HTML_To_PDF_Demo/src/js/Html2Pdf_2.js
+++ HTML_To_PDF_Demo/src/js/Html2Pdf_2.js
@@ -2,41 +2,51 @@ import html2canvas from "../lib/html2canvas/html2canvas.esm.js"
 import jsPDF from "../lib/jspdf/jspdf.es.js"
 import eventBus from '../lib/eventBus/event.js'
 /* 
- * 将页面导出为pdf
- * @param elem 需要导出的dom，默认document>body
+ * 将页面多个元素导出为pdf
+ * @param elem 需要导出的dom列表，默认document>body
  * @param scale 缩放程度，清晰度，越大越清晰，图片也越大
  * @param fileName 导出文件名称
  * @param background 导出PDF的背景色
+ * @param direct 导出方向(pdf拼接方向)，horizontal：水平，vertical：垂直
  */
-export default class Html2Pdf {
-    constructor(elem, scale = 2, fileName = 'fileName', background = '#FFFFFF') {
-        if (!elem) {
-            elem = document.body
+export default class Html2Pdfs {
+    constructor(elems, scale = 2, fileName = 'fileName', background = '#FFFFFF', direct = 'vertical') {
+        if (!elems || !elems.length) {
+            elems = [document.body]
         }
         this.canvas = null
-        this.init(elem, scale, fileName, background)
+        this.init(elems, scale, fileName, background, direct)
     }
-    init(elem, scale, fileName, background) {
+    init(elem, scale, fileName, background, direct) {
         eventBus.onEvent('print', async (e) => {
-            this.canvas = await html2canvas(elem, { //截取标签转换为canvas
-                canvas: this.createCanvas(elem, scale),
-                background,
-                scale: 1, //新版与之前的一定要设置scale，不然会导致成像缩放(重要)
-            })
+            this.canvas = await this.outputElem(elem, scale, background)
         })
         eventBus.onEvent('showEle', async (e) => {
-            this.canvas && canvasBox.appendChild(this.canvas) //显示效果
+            this.canvas && this.canvas.forEach(_ => canvasBox.appendChild(_)) //显示效果
             pdfEle.style.display = 'none'
         })
         eventBus.onEvent('download', async (e) => {
-            this.canvas && this.downloadPdf(this.canvas, fileName, scale)
+            this.canvas && this.downloadPdf(this.canvas, fileName, scale, direct)
         })
     }
-    downloadPdf(canvas, fileName, scale) { //将canvas变成PDF并下载
-        const size = [canvas.width / scale, canvas.height / scale] //pdf真实宽高
-        //第一个参数表示横向与纵向，具体可看文档，我这里做了一个适配，宽比高长则是横向反之则是纵向
-        const doc = new jsPDF(size[0] / size[1] > 1 ? 'l' : 'p', 'px', size)
-        doc.addImage(canvas.toDataURL('image/jpeg', 1.0), 'JPEG', 0, 0, ...size) //将canvas转换为图片并添加到jsPDF中
+    downloadPdf(canvas, fileName, scale, direct) { //将canvas变成PDF并下载
+        let height, width
+        if (direct === 'horizontal') {
+            width = this.getSum(canvas, 'width')
+            height = this.getLongestItem(canvas.filter(_ => _.height))
+        } else if (direct === 'vertical') {
+            height = this.getSum(canvas, 'height')
+            width = this.getLongestItem(canvas.filter(_ => _.width))
+        }
+
+        console.log(width, height)
+        canvas.forEach(_ => {
+            const size = [canvas.width / scale, canvas.height / scale] //pdf真实宽高
+            //第一个参数表示横向与纵向，具体可看文档，我这里做了一个适配，宽比高长则是横向反之则是纵向
+            const doc = new jsPDF(size[0] / size[1] > 1 ? 'l' : 'p', 'px', size)
+            doc.addImage(canvas.toDataURL('image/jpeg', 1.0), 'JPEG', 0, 0, ...size) //将canvas转换为图片并添加到jsPDF中
+        })
+
         doc.save(fileName + ".pdf"); //保存PDF
     }
 
@@ -51,5 +61,18 @@ export default class Html2Pdf {
         context.scale(scale, scale); //等比缩放
         // context.translate(-(clientRect ?.left), -(clientRect ?.top)); //通过translate取反位移
         return canvas
+    }
+    outputElem(elems, scale, background) {
+        return Promise.all(elems.map(_ => html2canvas(_, { //截取标签转换为canvas
+            canvas: this.createCanvas(_, scale),
+            background,
+            scale: 1, //新版与之前的一定要设置scale，不然会导致成像缩放(重要)
+        })))
+    }
+    getSum(list, key) {
+        return list.reduce((total, item) => (total[key] || total) + item[key])
+    }
+    getLongestItem(list) {
+        return Math.max.apply(null, list)
     }
 }
