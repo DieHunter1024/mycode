60e900f49825386e66077f82dcb242c2caa9dd32
zhangboyu <zhangboyu@hosecloud.com>
2021-07-04 23:49:38 +0800

    将页面多个元素合并并导出为pdf

--- HTML_To_PDF_Demo/src/js/Html2Pdf_2.js
+++ HTML_To_PDF_Demo/src/js/Html2Pdf_2.js
@@ -2,7 +2,7 @@ import html2canvas from "../lib/html2canvas/html2canvas.esm.js"
 import jsPDF from "../lib/jspdf/jspdf.es.js"
 import eventBus from '../lib/eventBus/event.js'
 /* 
- * 将页面多个元素导出为pdf
+ * 将页面多个元素合并并导出为pdf
  * @param elem 需要导出的dom列表，默认document>body
  * @param scale 缩放程度，清晰度，越大越清晰，图片也越大
  * @param fileName 导出文件名称
@@ -26,30 +26,38 @@ export default class Html2Pdfs {
             pdfEle.style.display = 'none'
         })
         eventBus.onEvent('download', async (e) => {
-            this.canvas && this.downloadPdf(this.canvas, fileName, scale, direct)
+            this.canvas && this.downloadPdf(this.createPDFConfig(this.canvas, scale, direct), fileName)
         })
     }
-    downloadPdf(canvas, fileName, scale, direct) { //将canvas变成PDF并下载
-        let height, width
-        if (direct === 'horizontal') {
-            width = this.getSum(canvas, 'width')
-            height = this.getLongestItem(canvas.filter(_ => _.height))
-        } else if (direct === 'vertical') {
-            height = this.getSum(canvas, 'height')
-            width = this.getLongestItem(canvas.filter(_ => _.width))
-        }
-
-        console.log(width, height)
-        canvas.forEach(_ => {
-            const size = [canvas.width / scale, canvas.height / scale] //pdf真实宽高
-            //第一个参数表示横向与纵向，具体可看文档，我这里做了一个适配，宽比高长则是横向反之则是纵向
-            const doc = new jsPDF(size[0] / size[1] > 1 ? 'l' : 'p', 'px', size)
-            doc.addImage(canvas.toDataURL('image/jpeg', 1.0), 'JPEG', 0, 0, ...size) //将canvas转换为图片并添加到jsPDF中
+    createPDFConfig(canvas, scale, direct) { //配置canvas
+        let x = 0,
+            y = 0,
+            width = 0,
+            height = 0
+        // horizontal：水平，vertical：垂直
+        canvas = canvas.map((_, i) => {
+            if (direct === 'horizontal') { //水平方向，总宽度叠加，高度取最高值，x取前者宽度
+                width += _.width
+                height = height > _.height ? height : _.height
+                x += canvas[i - 1]?.width || 0
+            } else if (direct === 'vertical') { //垂直方向，总高度叠加，宽度取最长值，y取前者宽度
+                height += _.height
+                width = width > _.width ? width : _.width
+                y += canvas[i - 1]?.height || 0
+            }
+            _.config = [_, '', x / scale, y / scale, _.width / scale, _.height / scale]
+            return _
         })
-
+        canvas.__proto__.size = [width / scale, height / scale] //pdf真实宽高
+        return canvas
+    }
+    downloadPdf(canvas, fileName) { //将canvas变成PDF并下载
+        //第一个参数表示横向与纵向，具体可看文档，我这里做了一个适配，宽比高长则是横向反之则是纵向
+        const doc = new jsPDF(canvas.size[0] / canvas.size[1] > 1 ? 'l' : 'p', 'px', canvas.size)
+        //将canvas添加到jsPDF中
+        canvas.forEach(_ => doc.addImage(..._.config))
         doc.save(fileName + ".pdf"); //保存PDF
     }
-
     createCanvas(target, scale) { //target是待截取的标签，我们通过target生成对应大小的canvas
         let canvas = document.createElement("canvas");
         let context = canvas.getContext("2d")
@@ -67,12 +75,7 @@ export default class Html2Pdfs {
             canvas: this.createCanvas(_, scale),
             background,
             scale: 1, //新版与之前的一定要设置scale，不然会导致成像缩放(重要)
+            logging: false
         })))
-    }
-    getSum(list, key) {
-        return list.reduce((total, item) => (total[key] || total) + item[key])
-    }
-    getLongestItem(list) {
-        return Math.max.apply(null, list)
     }
 }
