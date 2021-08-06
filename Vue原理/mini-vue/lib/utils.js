64aaa222690e4d221c180ec65465073174804c2a
zhangboyu <zhangboyu@hosecloud.com>
2021-08-06 14:57:07 +0800

    setDeepData注释

--- Vue原理/mini-vue/lib/utils.js
+++ Vue原理/mini-vue/lib/utils.js
@@ -12,7 +12,7 @@ function getDeepData(object, path, defaultValue) {
 //lodash中的 _.set()，赋值对象某级属性
 function setDeepData(object, path, value) {
     const paths = path.split('.')
-    const last = paths[paths.length - 1]
+    const last = paths[paths.length - 1]//为何要在length - 1时赋值：因为object的引用关系使得我们可以一级一级赋值，而当最后一项是基本类型时，无法将引用的值赋给原始的object
     let _obj = object
     for (const i of paths) {
         last === i && (_obj[last] = value)
