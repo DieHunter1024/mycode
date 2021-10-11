56854af9f94a6e2d43620c6ddb2adf6421c45ca4
zhangboyu <zhangboyu@hosecloud.com>
2021-10-11 17:56:25 +0800

    优化登录

--- AutoJs/脚本/薪人薪事打卡/main.js
+++ AutoJs/脚本/薪人薪事打卡/main.js
@@ -1,16 +1,19 @@
 var appName = "薪人薪事", //app名
   packageName = getPackageName(appName), //包名
-  cardViewBtn = "ll_clock", //打卡界面按钮
-  cardTakeBtn = "rl_my_clock_to_clock_in", //打卡按钮
-  userLoginBtn = "tv_password_login", //账号密码登录按钮
-  cbAgreeCheck = "cb_agree", //同意选项
-  userInput = "et_phone", //用户名输入框
-  pwdInput = "id_home_content_spet_phone", //密码输入框
-  events = events.emitter(),
+  cardViewBtn = () => id("ll_clock").findOne(), //打卡界面按钮
+  cardTakeBtn = () => id("rl_my_clock_to_clock_in").findOne(), //打卡按钮
+  userLoginBtn = () => id("tv_password_login").findOne(), //账号密码登录按钮
+  cbAgreeCheck = () => id("cb_agree").findOne(), //同意选项
+  userInput = () => id("et_phone").findOne(), //用户名输入框
+  pwdInput = () => id("et_password").findOne(), //密码输入框
+  submit = () => id("btn_login").findOne(), //登录按钮
+  bus = events.emitter();
 const EventName = {
-  TakeCard: "TakeCard", //打卡完成
-  AppLoad: "AppLoad", //App加载完成
-};
+    TakeCard: "TakeCard", //打卡完成
+    AppLoad: "AppLoad", //App加载完成
+  },
+  userName = "123",
+  passWord = "123";
 
 init();
 
@@ -19,65 +22,60 @@ function init() {
   toast("launchApp:" + appName);
   console.log("launchApp:" + appName, launchApp(appName));
   waitForPackage(packageName);
-  events.emit(EventName.AppLoad);
-  // openCardView();
-  // sleep(1000);
-  // takeCard(exitApp);
+  bus.emit(EventName.AppLoad);
 }
 function checkLogin() {
-  var userLogin = id(userLoginBtn).findOne(),
-    cbAgree = id(cbAgreeCheck).findOne();
-  if (userLogin && cbAgree) {
-    login(userLogin, cbAgree);
+  if (id("tv_password_login").find().size() > 0) {
+    console.log("userLogin", userLoginBtn().click());
+    login();
     return;
   }
   openCardView();
-
-  // cbAgree && !cbAgree.checked() && console.log("cbAgree", cbAgree.click());
 }
-function login(userLogin, cbAgree) {
-  console.log("userLogin", userLogin.click());
-  var user = id(userInput).findOne(),
-    pwd = id(pwdInput).findOne();
-    user.setText('text')
+function login() {
+  id("cb_agree").waitFor();
+  var cbAgree = cbAgreeCheck();
+  if (!cbAgree.checked()) {
+    console.log("cbAgree", cbAgree.click());
+  }
+  console.log("userName", userInput().setText(userName));
+  console.log("passWord", pwdInput().setText(passWord));
+  console.log("submit", submit().click());
+  toast("submit");
+  openCardView();
 }
 function openCardView() {
   toast("打卡界面按钮click");
-  var cardButton = id(cardViewBtn).findOne();
+  var cardButton = cardViewBtn();
   console.log("打卡界面按钮click", cardButton.click());
+  exitApp();
 }
 
 function takeCard() {
   var takeCardButton;
   var timer = setInterval(function takeCardTick() {
-    takeCardButton = id(cardTakeBtn).findOne();
+    takeCardButton = cardTakeBtn();
     console.log("clickable", takeCardButton.clickable());
     if (takeCardButton.clickable()) {
       console.log("打卡按钮click", takeCardButton.click());
-      events.emit(EventName.TakeCard);
+      bus.emit(EventName.TakeCard);
       clearInterval(timer);
     }
   }, 1000);
 }
 
 function exitApp() {
-  sleep(2000);
-  toast("back");
-  console.log("back", back());
-  sleep(300);
-  console.log("back", back());
-  sleep(300);
-  console.log("back", back());
-  sleep(300);
-  console.log("back", back());
+  var sh = new Shell(true);
+  sh.exec("am force-stop" + " " + packageName);
+  sleep(1000);
+  sh.exit();
 }
 
 function regEvent() {
-  events.on(EventName.AppLoad, function () {
+  bus.on(EventName.AppLoad, function () {
     console.log("launchAppSuccess", packageName);
     toast("launchAppSuccess", packageName);
     checkLogin();
-    // openCardView();
   });
-  events.on(EventName.TakeCard, exitApp);
+  bus.on(EventName.TakeCard, exitApp);
 }
