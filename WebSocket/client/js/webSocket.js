d3595facc51c6d7f723f1f99acc3df0ff281a6c1
zhangboyu <zhangboyu@hosecloud.com>
2022-04-21 23:15:38 +0800

    fix: 变量定义优化，关闭后无法重新打开

--- WebSocket/client/js/webSocket.js
+++ WebSocket/client/js/webSocket.js
@@ -1,113 +1,117 @@
-import eventBus
-    from "./eventBus.js"
+import eventBus from "./eventBus.js";
 
-const ModeCode = {//websocket消息类型
-    MSG: 'message',//普通消息
-    HEART_BEAT: 'heart_beat'//心跳
-}
+const ModeCode = {
+  //websocket消息类型
+  MSG: "message", //普通消息
+  HEART_BEAT: "heart_beat", //心跳
+};
 
 export default class MyWebSocket extends WebSocket {
-    constructor (url, protocols) {
-        super(url, protocols);
-        return this
-    }
+  constructor(url, protocols) {
+    super(url, protocols);
+    return this;
+  }
 
-    /*
-     * 入口函数
-     * @param heartBeatConfig  time：心跳时间间隔 timeout：心跳超时间隔 reconnect：断线重连时间间隔
-     * @param isReconnect 是否断线重连
-     */
-    init (heartBeatConfig, isReconnect) {
-        this.onopen = this.openHandler//连接上时回调
-        this.onclose = this.closeHandler//断开连接时回调
-        this.onmessage = this.messageHandler//收到服务端消息
-        this.onerror = this.errorHandler//连接出错
-        this.heartBeat = heartBeatConfig
-        this.isReconnect = isReconnect
-        this.reconnectTimer = null//断线重连时间器
-        this.webSocketState = false//socket状态 true为已连接
-    }
+  /*
+   * 入口函数
+   * @param heartBeatConfig  time：心跳时间间隔 timeout：心跳超时间隔 reconnect：断线重连时间间隔
+   * @param isReconnect 是否断线重连
+   */
+  init(heartBeatConfig, isReconnect) {
+    this.onopen = this.openHandler; //连接上时回调
+    this.onclose = this.closeHandler; //断开连接时回调
+    this.onmessage = this.messageHandler; //收到服务端消息
+    this.onerror = this.errorHandler; //连接出错
+    this.heartBeat = heartBeatConfig;
+    this.isReconnect = isReconnect;
+    this.reconnectTimer = null; //断线重连时间器
+    this.webSocketState = false; //socket状态 true为已连接
+  }
 
-    openHandler () {
-        eventBus.emitEvent('changeBtnState', 'open')//触发事件改变按钮样式
-        this.webSocketState = true//socket状态设置为连接，做为后面的断线重连的拦截器
-        this.heartBeat && this.heartBeat.time ? this.startHeartBeat(this.heartBeat.time) : ""//是否启动心跳机制
-        console.log('开启')
-    }
+  openHandler() {
+    eventBus.emitEvent("changeBtnState", "open"); //触发事件改变按钮样式
+    this.webSocketState = true; //socket状态设置为连接，做为后面的断线重连的拦截器
+    this.heartBeat && this.heartBeat.time
+      ? this.startHeartBeat(this.heartBeat.time)
+      : ""; //是否启动心跳机制
+    console.log("开启");
+  }
 
-    messageHandler (e) {
-        let data = this.getMsg(e)
-        switch(data.ModeCode) {
-            case ModeCode.MSG://普通消息
-                console.log('收到消息' + data.msg)
-                break;
-            case ModeCode.HEART_BEAT://心跳
-                this.webSocketState = true
-                console.log('收到心跳响应' + data.msg)
-                break;
-        }
+  messageHandler(e) {
+    const data = this.getMsg(e);
+    switch (data.ModeCode) {
+      case ModeCode.MSG: //普通消息
+        console.log("收到消息" + data.msg);
+        break;
+      case ModeCode.HEART_BEAT: //心跳
+        this.webSocketState = true;
+        console.log("收到心跳响应" + data.msg);
+        break;
     }
+  }
 
-    closeHandler () {//socket关闭
-        eventBus.emitEvent('changeBtnState', 'close')//触发事件改变按钮样式
-        this.webSocketState = false//socket状态设置为断线
-        console.log('关闭')
-    }
+  closeHandler() {
+    //socket关闭
+    eventBus.emitEvent("changeBtnState", "close"); //触发事件改变按钮样式
+    this.webSocketState = false; //socket状态设置为断线
+    console.log("关闭");
+  }
 
-    errorHandler () {//socket出错
-        eventBus.emitEvent('changeBtnState', 'close')//触发事件改变按钮样式
-        this.webSocketState = false//socket状态设置为断线
-        this.reconnectWebSocket()//重连
-        console.log('出错')
-    }
+  errorHandler() {
+    //socket出错
+    eventBus.emitEvent("changeBtnState", "close"); //触发事件改变按钮样式
+    this.webSocketState = false; //socket状态设置为断线
+    this.reconnectWebSocket(); //重连
+    console.log("出错");
+  }
 
-    sendMsg (obj) {
-        this.send(JSON.stringify(obj))
-    }
+  sendMsg(obj) {
+    this.send(JSON.stringify(obj));
+  }
 
-    getMsg (e) {
-        return JSON.parse(e.data)
-    }
+  getMsg(e) {
+    return JSON.parse(e.data);
+  }
 
-    /*
-     * 心跳初始函数
-     * @param time：心跳时间间隔
-     */
-    startHeartBeat (time) {
-        setTimeout(() => {
-            this.sendMsg({
-                ModeCode: ModeCode.HEART_BEAT,
-                msg: new Date()
-            })
-            this.waitingServer()
-        }, time)
-    }
+  /*
+   * 心跳初始函数
+   * @param time：心跳时间间隔
+   */
+  startHeartBeat(time) {
+    setTimeout(() => {
+      this.sendMsg({
+        ModeCode: ModeCode.HEART_BEAT,
+        msg: new Date(),
+      });
+      this.waitingServer();
+    }, time);
+  }
 
-    //延时等待服务端响应，通过webSocketState判断是否连线成功
-    waitingServer () {
-        this.webSocketState = false
-        setTimeout(() => {
-            if(this.webSocketState) {
-                this.startHeartBeat(this.heartBeat.time)
-                return
-            }
-            console.log('心跳无响应，已断线')
-            try {
-                this.close()
-            } catch(e) {
-                console.log('连接已关闭，无需关闭')
-            }
-            this.reconnectWebSocket()
-        }, this.heartBeat.timeout)
-    }
+  //延时等待服务端响应，通过webSocketState判断是否连线成功
+  waitingServer() {
+    this.webSocketState = false;
+    setTimeout(() => {
+      if (this.webSocketState) {
+        this.startHeartBeat(this.heartBeat.time);
+        return;
+      }
+      console.log("心跳无响应，已断线");
+      try {
+        this.close();
+      } catch (e) {
+        console.log("连接已关闭，无需关闭");
+      }
+      this.reconnectWebSocket();
+    }, this.heartBeat.timeout);
+  }
 
-    //重连操作
-    reconnectWebSocket () {
-        if(!this.isReconnect) {
-            return;
-        }
-        this.reconnectTimer = setTimeout(() => {
-            eventBus.emitEvent('reconnect')
-        }, this.heartBeat.reconnect)
+  //重连操作
+  reconnectWebSocket() {
+    if (!this.isReconnect) {
+      return;
     }
+    this.reconnectTimer = setTimeout(() => {
+      eventBus.emitEvent("reconnect");
+    }, this.heartBeat.reconnect);
+  }
 }
--- WebSocket/server/server.js
+++ WebSocket/server/server.js
@@ -1,91 +1,97 @@
-const http = require('http');
-const WebSocket = require('ws');
-const port = 1024//端口
-const pathname = '/ws/'//访问路径
-const server = http.createServer()
+const http = require("http");
+const WebSocket = require("ws");
+const port = 1024; //端口
+const pathname = "/ws/"; //访问路径
+const server = http.createServer();
 
 class WebSocketServer extends WebSocket.Server {
-    constructor () {
-        super(...arguments);
-        this.webSocketClient = {}//存放已连接的客户端
-    }
+  constructor() {
+    super(...arguments);
+    this.webSocketClient = {}; //存放已连接的客户端
+  }
 
-    set ws (val) {//代理当前的ws，赋值时将其初始化
-        this._ws = val
-        val.t = this;
-        val.on('error', this.errorHandler)
-        val.on('close', this.closeHandler)
-        val.on('message', this.messageHandler)
-    }
+  set ws(val) {
+    //代理当前的ws，赋值时将其初始化
+    this._ws = val;
+    val.t = this;
+    val.on("error", this.errorHandler);
+    val.on("close", this.closeHandler);
+    val.on("message", this.messageHandler);
+  }
 
-    get ws () {
-        return this._ws
-    }
+  get ws() {
+    return this._ws;
+  }
 
-    messageHandler (e) {
-        console.info('接收客户端消息')
-        let data = JSON.parse(e)
-        switch(data.ModeCode) {
-            case 'message':
-                console.log('收到消息' + data.msg)
-                this.send(e)
-                break;
-            case 'heart_beat':
-                console.log(`收到${this.name}心跳${data.msg}`)
-                this.send(e)
-                break;
-        }
+  messageHandler(e) {
+    console.info("接收客户端消息");
+    const data = JSON.parse(e);
+    switch (data.ModeCode) {
+      case "message":
+        console.log("收到消息" + data.msg);
+        this.send(e);
+        break;
+      case "heart_beat":
+        console.log(`收到${this.name}心跳${data.msg}`);
+        this.send(e);
+        break;
     }
+  }
 
-    errorHandler (e) {
-        this.t.removeClient(this)
-        console.info('客户端出错')
-    }
+  errorHandler(e) {
+    this.t.removeClient(this);
+    console.info("客户端出错");
+  }
 
-    closeHandler (e) {
-        this.t.removeClient(this)
-        console.info('客户端已断开')
-    }
+  closeHandler(e) {
+    this.t.removeClient(this);
+    console.info("客户端已断开");
+  }
 
-    addClient (item) {//设备上线时添加到客户端列表
-        if(this.webSocketClient[item['name']]) {
-            console.log(item['name'] + '客户端已存在')
-            this.webSocketClient[item['name']].close()
-        }
-        console.log(item['name'] + '客户端已添加')
-        this.webSocketClient[item['name']] = item
+  addClient(item) {
+    //设备上线时添加到客户端列表
+    if (this.webSocketClient[item["name"]]) {
+      console.log(item["name"] + "客户端已存在");
+      this.webSocketClient[item["name"]].close();
     }
+    console.log(item["name"] + "客户端已添加");
+    this.webSocketClient[item["name"]] = item;
+  }
 
-    removeClient (item) {//设备断线时从客户端列表删除
-        if(!this.webSocketClient[item['name']]) {
-            console.log(item['name'] + '客户端不存在')
-            return;
-        }
-        console.log(item['name'] + '客户端已移除')
-        this.webSocketClient[item['name']] = null
+  removeClient(item) {
+    //设备断线时从客户端列表删除
+    if (!this.webSocketClient[item["name"]]) {
+      console.log(item["name"] + "客户端不存在");
+      return;
     }
+    console.log(item["name"] + "客户端已移除");
+    this.webSocketClient[item["name"]] = null;
+  }
 }
 
-const webSocketServer = new WebSocketServer({noServer: true})
-server.on("upgrade", (req, socket, head) => {//通过http.server过滤数据
-    let url = new URL(req.url, `http://${req.headers.host}`)
-    let name = url.searchParams.get('name')//获取连接标识
-    if(!checkUrl(url.pathname, pathname)) {//未按标准
-        socket.write('未按照标准访问');
-        socket.destroy();
-        return;
-    }
-    webSocketServer.handleUpgrade(req, socket, head, function (ws) {
-        ws.name = name//添加索引，方便在客户端列表查询某个socket连接
-        webSocketServer.addClient(ws);
-        webSocketServer.ws = ws
-    });
-})
+const webSocketServer = new WebSocketServer({ noServer: true });
+server.on("upgrade", (req, socket, head) => {
+  //通过http.server过滤数据
+  const url = new URL(req.url, `http://${req.headers.host}`);
+  const name = url.searchParams.get("name"); //获取连接标识
+  if (!checkUrl(url.pathname, pathname)) {
+    //未按标准
+    socket.write("未按照标准访问");
+    socket.destroy();
+    return;
+  }
+  webSocketServer.handleUpgrade(req, socket, head, function (ws) {
+    ws.name = name; //添加索引，方便在客户端列表查询某个socket连接
+    webSocketServer.addClient(ws);
+    webSocketServer.ws = ws;
+  });
+});
 server.listen(port, () => {
-    console.log('服务开启')
-})
+  console.log("服务开启");
+});
 
 //验证url标准
-function checkUrl (url, key) {//判断url是否包含key
-    return - ~ url.indexOf(key)
+function checkUrl(url, key) {
+  //判断url是否包含key
+  return -~url.indexOf(key);
 }
