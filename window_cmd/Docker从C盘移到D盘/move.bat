ccddfb81782e9634668816a833cb3fec5d34a63e
zhangboyu <zhangboyu@hosecloud.com>
2022-04-29 21:26:17 +0800

    feat：整理window命令

---
+++ window_cmd/Docker从C盘移到D盘/move.bat
@@ -0,0 +1,5 @@
+mklink /j "C:\ProgramData\Docker" "D:\Soft\Docker\Docker"
+mklink /j "C:\ProgramData\DockerDesktop" "D:\Soft\Docker\DockerDesktop"
+mklink /j "C:\Program Files\Docker" "D:\Program Files\Docker"
+mklink /j "C:\Users\HOSE\AppData\Local\Docker" "D:\Users\HOSE\AppData\Local\Docker"
+PAUSE
---
+++ window_cmd/Docker从C盘移到D盘/remove.bat
@@ -0,0 +1,5 @@
+rd /s /q "C:\ProgramData\Docker"
+rd /s /q "C:\ProgramData\DockerDesktop"
+rd /s /q "C:\Program Files\Docker"
+rd /s /q "C:\Users\HOSE\AppData\Local\Docker"
+PAUSE
