d048e3585569c310fadd9c1d3000a3a6d2fdd954
zhangboyu <zhangboyu@hosecloud.com>
2022-05-23 18:33:19 +0800

    feat: proxy重写

--- Vue原理/mini-vue-proxy-temp/js/vue-demo.js
+++ Vue原理/mini-vue-proxy-temp/js/vue-demo.js
@@ -1,19 +1,15 @@
 class VueDemo {
   constructor(vmEle, options) {
     this.options = options; //配置信息
-    this.$data = options.data;
+    this.$data = new Observer(options.data);
     // 判断options.el是否存在
     (this.el = options.el) &&
       Object.defineProperties(this, {
-        //observer和compile的顺序不要错，否则监听不到compile中的数据
-        observer: {
-          value: new Observer(options.data), // 数据监听器
-        },
         proxy: {
-          value: new DataProxy(options.data, this), //data代理到this
+          value: new DataProxy(this.$data, this), //data代理到this
         },
         compile: {
-          value: new Compile(vmEle, options.el, this), //指令解析器
+          value: new Compile(vmEle, this.options.el, this), //指令解析器
         },
       });
   }
--- Vue原理/mini-vue-proxy-temp/lib/observer.js
+++ Vue原理/mini-vue-proxy-temp/lib/observer.js
@@ -1,28 +1,35 @@
 // 发布模式
 class Observer {
-    constructor(data) {
-        this.initObserver(data)
+  constructor(data) {
+    return this.initObserver({ data })?.data;
+  }
+  // 劫持所有数据
+  initObserver(data) {
+    let _data = {};
+    if (this.checkObject(data)) {
+      for (const key in data) {
+        _data[key] = this.defineReactive(data[key]);
+      }
     }
-    // 劫持所有数据
-    initObserver(data) {
-        if (data && typeof data === 'object') {
-            for (const key in data) {
-                this.defineReactive(data, key, data[key])
-            }
-        }
-    }
-    // 响应拦截器，递归监听所有层级
-    defineReactive(data, key, val) {
-        this.initObserver(val) //劫持子项
-        const dep = new Dep() //将observer与watcher连接，当watcher触发数据变化后，将watcher中的回调函数注册到dep中
-        Object.defineProperty(data, key, {
-            enumerable: true, // 允许枚举
-            configurable: false, // 不能被定义
-            get: _ => {
-                Dep.target && dep.subscribe(Dep.target); //获取属性值时,将watcher中的回调函数注册到dep中（在页面初始化时调用）
-                return val
-            },
-            set: newVal => newVal !== val && (val = newVal, this.initObserver(newVal), dep.fireEvent()) //设置属性时，对比新值和旧值有无差别，若修改的值是引用型时，将属性重新注册到dep中,并更新视图
-        })
-    }
+    return _data;
+  }
+  checkObject(val) {
+    return val && typeof val === "object";
+  }
+  // 响应拦截器，递归监听所有层级
+  defineReactive(val) {
+    if (!this.checkObject(val)) return val;
+    val = this.initObserver(val); //劫持子项
+    const dep = new Dep(); //将observer与watcher连接，当watcher触发数据变化后，将watcher中的回调函数注册到dep中
+    return new Proxy(val, {
+      set(target, key, newVal) {
+        newVal !== target[key] && ((target[key] = newVal), dep.fireEvent()); //设置属性时，对比新值和旧值有无差别，若修改的值是引用型时，将属性重新注册到dep中,并更新视图
+        return Reflect.set(...arguments);
+      },
+      get() {
+        Dep.target && dep.subscribe(Dep.target); //获取属性值时,将watcher中的回调函数注册到dep中（在页面初始化时调用）
+        return Reflect.get(...arguments);
+      },
+    });
+  }
 }
