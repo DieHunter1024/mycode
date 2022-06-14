33b1955a139e15b8403f068f7b32516a703d1570
zhangboyu <zhangboyu@hosecloud.com>
2022-06-14 18:01:37 +0800

    fix: setDeepData优化空指针状态

--- Vue原理/mini-vue-proxy-temp/lib/utils.js
+++ Vue原理/mini-vue-proxy-temp/lib/utils.js
@@ -13,11 +13,18 @@ function getDeepData(object, path, defaultValue) {
 function setDeepData(object, path, value) {
     const paths = path.split('.')
     const last = paths[paths.length - 1]//为何要在length - 1时赋值：因为object的引用关系使得我们可以一级一级赋值，而当最后一项是基本类型时，无法将引用的值赋给原始的object
-    let _obj = object
+    let _object = object
     for (const i of paths) {
-        last === i && (_obj[last] = value)
-        _obj = _obj[i]
+        if (typeof _object !== "object" && _object !== undefined) {
+            return object
+        }
+        !_object && (_object = {})
+        !_object[i] && (_object[i] = {})
+        last === i && (_object[last] = value)
+        _object = _object[i]
+
     }
+    return object
 }
 // 移除属性值
 function removeAttr(elem, key) {
